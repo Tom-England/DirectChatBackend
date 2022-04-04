d04f3e5acb218123915235a712a95c91c9a80137
Tom-England <tomengland6679@gmail.com>
2022-04-04 19:14:06 +0100

    Added all tables I think i'll need

--- DatabaseHandler.cs
+++ DatabaseHandler.cs
@@ -7,7 +7,26 @@ namespace Storage
 		SQLiteConnection db_connection;
 		public void setup(){
 			SQLiteConnection.CreateFile("MyDatabase.sqlite");
-			string setup = "CREATE TABLE messages";
+			string setup = @"CREATE TABLE messages
+			(
+				message_id INT PRIMARY KEY NOT NULL,
+				message_text CHAR(" + Network.Constants.MESSAGE_SIZE + @") NOT NULL,
+				sender_id INT NOT NULL,
+				FOREIGN KEY(sender_id) REFERENCES users(user_id)
+			)
+			
+			CREATE TABLE users
+			(
+				user_id INT PRIMARY KEY NOT NULL,
+				user_name VARCHAR(20)
+			)
+
+			CREATE TABLE password
+			(
+				password_hash CHAR(20);
+			)
+			
+			)";
 		}
 
 		public void connect(){
