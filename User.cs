e2b0974e14cd6a94e09e82960beb8695c667fdeb
Tom-England <tomengland6679@gmail.com>
2022-04-23 20:19:45 +0100

    Added user transferrable type

--- User.cs
+++ User.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Runtime.InteropServices; // For converting the message to a byte array
 
 namespace Network{
 	class User{
@@ -17,7 +19,50 @@ namespace Network{
 			id = Guid.NewGuid();
 			Name = _name;
 		}
+		private byte[] key;
+		public byte[] Key{
+			get { return key; }
+			set { key = value; }
+		}
 
-	
+		public struct UserTransferable{
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = Constants.MESSAGE_SIZE)]
+			string name;
+			Guid id;
+			byte[] key;
+			byte[] iv;
+
+			public UserTransferable(string _name, Guid _id, byte[] _key, byte[] _iv){
+				name = _name;
+				id = _id;
+				key = _key;
+				iv = _iv;
+			}
+		}
+
+		public byte[] get_bytes(UserTransferable u) {
+			int size = Marshal.SizeOf(u);
+			byte[] arr = new byte[size];
+
+			IntPtr ptr = Marshal.AllocHGlobal(size);
+			Marshal.StructureToPtr(u, ptr, true);
+			Marshal.Copy(ptr, arr, 0, size);
+			Marshal.FreeHGlobal(ptr);
+			return arr;
+		}
+
+		public UserTransferable from_bytes(byte[] arr) {
+			UserTransferable u = new UserTransferable();
+
+			int size = Marshal.SizeOf(u);
+			IntPtr ptr = Marshal.AllocHGlobal(size);
+
+			Marshal.Copy(arr, 0, ptr, size);
+
+			u = (UserTransferable)Marshal.PtrToStructure(ptr, u.GetType());
+			Marshal.FreeHGlobal(ptr);
+
+			return u;
+		}
 	}
 }
