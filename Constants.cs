e62982b81348a20e98e57e4848ab4627e2b6b22d
Tom-England <tomengland6679@gmail.com>
2022-05-06 03:44:16 +0100

    Changed port

--- Constants.cs
+++ Constants.cs
@@ -8,6 +8,6 @@ namespace Network{
 		public const int USER_STRUCT_SIZE = 228;
         public const string ACK = "ACK";
         public const string IP = "192.168.0.21";
-        public const Int32 PORT = 6679;
+        public const Int32 PORT = 41000;
     }
 }
